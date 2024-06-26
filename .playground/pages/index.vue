e5461e18253ca759ad4e8f94ddf518e717815948
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2024-06-26 20:26:05 +0100

    Another tweak to try to get packages running

---
+++ .playground/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Index.vue in playground</h1>
+        <FormsInputText />
+    </div>
+</template>
--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p>Root app.vue</p>
     <FormsInputText />
   </div>
 </template>
