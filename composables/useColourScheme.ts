6f4419e5c9752fae9cd345bc1e5e8d14e90ef24b
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2025-02-19 21:11:06 +0000

    Tidy up commented code within useColourScheme composable

--- composables/useColourScheme.ts
+++ composables/useColourScheme.ts
@@ -1,23 +1,5 @@
 export const useColourScheme = () => {
-  // const currentColourScheme = computed(() => {
-  //   return colourScheme.value;
-  // });
-
   const currentColourScheme = ref<'auto' | 'dark' | 'light' | null>(null);
-
-  // const updateColourScheme = (newColourScheme: 'auto' | 'dark' | 'light') => {
-  //   colourScheme.value = newColourScheme;
-  // };
-
-  // const getSetPrefereredColourScheme = () => {
-  //   if (import.meta.client) {
-  //     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !document.documentElement.dataset.colorScheme) {
-  //       colourScheme.value = 'dark';
-  //     } else {
-  //       colourScheme.value = 'light';
-  //     }
-  //   }
-  // };
 
   const returnSavedColourPreferenceFromLocalStorage = () => {
     if (import.meta.client) {
@@ -27,18 +9,6 @@ export const useColourScheme = () => {
 
   onMounted(() => {
     currentColourScheme.value = returnSavedColourPreferenceFromLocalStorage() || 'auto';
-    // colourScheme.value = currentColourScheme.value;
-    //
-    // if (import.meta.client) {
-    //   const savedColourScheme = localStorage.getItem('colourScheme');
-    //   if (savedColourScheme) {
-    //     colourScheme.value = savedColourScheme as 'dark' | 'light';
-    //   } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    //     colourScheme.value = 'dark';
-    //   } else {
-    //     colourScheme.value = 'light';
-    //   }
-    // }
   });
 
   watch(currentColourScheme, (newVal) => {
@@ -51,8 +21,5 @@ export const useColourScheme = () => {
 
   return {
     currentColourScheme,
-    // colourScheme,
-    // updateColourScheme,
-    // getSetPrefereredColourScheme,
   };
 };
