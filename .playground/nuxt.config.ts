4d25a7b8a201490ed62122e7d4432f28c5c61721
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2026-01-29 21:33:42 +0000

    add nuxt imports

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,4 +1,6 @@
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config"
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["./assets/styles/main.css"],
